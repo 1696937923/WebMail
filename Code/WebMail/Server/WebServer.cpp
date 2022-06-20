94d292d1d7a11f686bdcb4f9c7eab78e0b217a92
zxx <1696937923@qq.com>
2022-06-20 19:23:50 +0800

    update

--- Code/WebMail/Server/WebServer.cpp
+++ Code/WebMail/Server/WebServer.cpp
@@ -9,6 +9,7 @@
 #include <netinet/in.h>
 #include <cstdlib>
 #include <fcntl.h>
+#include "../ThreadPool/ThreadPool.cpp"
 using namespace std;
 
 #define BUFFERSIZE 1024
@@ -20,62 +21,78 @@ const char *Db_Name = "WebMail";
 const unsigned int Db_Port = 3306;
 const uint16_t Sock_Port = 6789;
 
+struct LOGIN_INFO;    //登录信息
+struct REGISTER_INFO; //注册信息
+struct QUERY_INFO;    //查询邮件信息
+struct PULL_INFO;     //拉取邮件信息
+struct BACK_INFO;     //返回信息
+int Login(struct LOGIN_INFO, MYSQL);
+int Register(struct REGISTER_INFO, MYSQL);
+int Query_Email(struct QUERY_INFO, MYSQL);
+int Pull_Email(struct PULL_INFO, MYSQL);
+
 class WebServer
 {
 public:
+    WebServer();
+    ~WebServer();
     void Run(); //启动服务器
 
 private:
     MYSQL mysql;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
-    struct REGISTER_INFO;
-    struct LOGIN_INFO;
-    struct QUERY_INFO;
-    struct PULL_INFO;
-    struct BACK_INFO;
+    int socket_fd;
+    ThreadPool tp;
 
     void Db_Connect();    //连接mysql数据库
     void Start_Service(); //开启服务器监听
     void Handle_Events(int, struct epoll_event *, int, int);
     void Do_Accept(int, int);
-    void Do_Deal(int, int, char *);
-    int Login(struct LOGIN_INFO);
-    int Register(struct REGISTER_INFO);
-    int Query_Email(struct QUERY_INFO);
-    int Pull_Email(struct PULL_INFO);
+    void Do_Deal(int, char *);
 };
 
-struct WebServer::REGISTER_INFO
+struct REGISTER_INFO
 {
     char email[48];
     char password[32];
 };
 
-struct WebServer::LOGIN_INFO
+struct LOGIN_INFO
 {
     char email[48];
     char password[32];
 };
 
-struct WebServer::QUERY_INFO
+struct QUERY_INFO
 {
 };
 
-struct WebServer::PULL_INFO
+struct PULL_INFO
 {
 };
 
-struct WebServer::BACK_INFO
+struct BACK_INFO
 {
     int code;
     char info[48];
 };
 
+WebServer::WebServer()
+{
+    // Run();
+}
+
+WebServer::~WebServer()
+{
+    mysql_close(&mysql);
+    tp.stop();
+    close(socket_fd);
+}
+
 void WebServer::Run()
 {
-    Db_Connect();
-    Start_Service();
+    Db_Connect();    //数据库连接
+    tp.start();      //开启线程池
+    Start_Service(); //开启socket端口监听
 }
 
 void WebServer::Db_Connect()
@@ -96,7 +113,7 @@ void WebServer::Start_Service()
     struct sockaddr_in s_addr;
     memset(&s_addr, 0, sizeof(sockaddr_in));
 
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     s_addr.sin_family = AF_INET;
     s_addr.sin_addr.s_addr = inet_addr(Host);
     s_addr.sin_port = htons(Sock_Port); //主机字节序转为网络字节序，s代表short
@@ -132,6 +149,20 @@ void WebServer::Start_Service()
     // 把网络格式的ip地址转为字符串形式
 }
 
+void WebServer::Handle_Events(int epoll_fd, struct epoll_event *events, int num, int socket_fd)
+{
+    int i, fd;
+    char buffer[BUFFERSIZE];
+    for (i = 0; i < num; i++)
+    {
+        fd = events[i].data.fd;                              //获得响应事件的描述符
+        if ((fd == socket_fd) && events[i].events & EPOLLIN) //对events和EPOLLIN“与”操作，判断这个文件描述符是否有input事件
+            Do_Accept(epoll_fd, fd);
+        else
+            Do_Deal(fd, buffer);
+    }
+}
+
 void WebServer::Do_Accept(int epoll_fd, int socket_fd)
 {
     struct sockaddr_in c_addr;
@@ -143,52 +174,58 @@ void WebServer::Do_Accept(int epoll_fd, int socket_fd)
     cout << inet_ntoa(c_addr.sin_addr) << ":" << c_addr.sin_port << " is connected!!" << endl;
 
     send(accept_fd, "ok", 3, 0);
-    // fcntl(accept_fd, F_SETFL, fcntl(accept_fd, F_GETFL) | O_NONBLOCK); //设置为非阻塞
     ev.data.fd = accept_fd;
     ev.events = EPOLLIN;
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, accept_fd, &ev); //继续添加事件
 }
 
-void WebServer::Do_Deal(int i, int fd, char *buffer)
+void WebServer::Do_Deal(int fd, char *buffer)
 {
+    char Msg_Tag[32];
     memset(buffer, 0, sizeof(buffer));
 
-    read(fd, buffer, sizeof(buffer));
-    // write(fd, buffer, strlen(buffer));
-    memset(buffer, 0, sizeof(buffer));
-    snprintf(buffer, sizeof(buffer), "%d", i);
-    for (int j = 0; j < 50; j++)
+    recv(fd, Msg_Tag, sizeof(Msg_Tag), 0);
+    if (strncmp(Msg_Tag, "Login", 5) == 0)
     {
-        send(fd, buffer, sizeof(buffer), 0);
-        sleep(0.5);
+        struct LOGIN_INFO login_info;
+        recv(fd, buffer, sizeof(buffer), 0);
+        memcpy(&login_info, buffer, sizeof(LOGIN_INFO));
+        tp.addTask(Login, login_info, mysql); //分配线程池任务
+        cout << "Login msg" << endl;
     }
-    // int ret = read(fd, buffer, sizeof(buffer) - 1);
-    // cout << buffer << endl;
-    // write(fd, "ok", 3);
+    else if (strncmp(Msg_Tag, "Register", 8) == 0)
+    {
+        // struct REGISTER_INFO register_info;
+        // recv(fd, buffer, sizeof(buffer), 0);
+        // memcpy(&register_info, buffer, sizeof(REGISTER_INFO));
+        // tp.addTask(Register, register_info);
+        // cout << "Register msg" << endl;
+    }
+    else if (strncmp(Msg_Tag, "Query", 5) == 0)
+    {
+        // struct QUERY_INFO query_info;
+        // recv(fd, buffer, sizeof(buffer), 0);
+        // memcpy(&query_info, buffer, sizeof(QUERY_INFO));
+        // tp.addTask(Query_Email, query_info);
+        // cout << "Query msg" << endl;
+    }
+    else if (strncmp(Msg_Tag, "Pull", 4) == 0)
+    {
+        // struct PULL_INFO pull_info;
+        // recv(fd, buffer, sizeof(buffer), 0);
+        // memcpy(&pull_info, buffer, sizeof(PULL_INFO));
+        // tp.addTask(Pull_Email, pull_info);
+        // cout << "Pull msg" << endl;
+    }
 }
 
-void WebServer::Handle_Events(int epoll_fd, struct epoll_event *events, int num, int socket_fd)
-{
-    int i, fd;
-    char buffer[BUFFERSIZE];
-    for (i = 0; i < num; i++)
-    {
-        fd = events[i].data.fd;                              //获得响应事件的描述符
-        if ((fd == socket_fd) && events[i].events & EPOLLIN) //对events和EPOLLIN“与”操作，判断这个文件描述符是否有input事件
-            Do_Accept(epoll_fd, fd);
-        else
-            Do_Deal(i, fd, buffer);
-    }
-}
-
-int WebServer::Login(struct LOGIN_INFO login_info)
+int Login(struct LOGIN_INFO login_info, MYSQL mysql)
 {
     string query_sql = "select password from User where email = '" + (string)login_info.email + "'";
     mysql_query(&mysql, query_sql.c_str());
-    res = mysql_store_result(&mysql);
-    row = mysql_fetch_row(res);
-
-    if (row[0] != login_info.password)
+    MYSQL_RES *res = mysql_store_result(&mysql);
+    MYSQL_ROW row = mysql_fetch_row(res);
+    if (strcmp(row[0], login_info.password) != 0)
     {
         cout << "(INFO)Login: Password error!" << endl;
         mysql_free_result(res);
@@ -199,11 +236,11 @@ int WebServer::Login(struct LOGIN_INFO login_info)
     return 1;
 }
 
-int WebServer::Register(struct REGISTER_INFO register_info)
+int Register(struct REGISTER_INFO register_info, MYSQL mysql)
 {
     string query_sql = "select * from User where email = '" + (string)register_info.email + "'";
     mysql_query(&mysql, query_sql.c_str());
-    res = mysql_store_result(&mysql);
+    MYSQL_RES *res = mysql_store_result(&mysql);
     if (res->row_count > 0)
     {
         cout << "(INFO)Register: User exists!" << endl;
@@ -223,12 +260,12 @@ int WebServer::Register(struct REGISTER_INFO register_info)
     return 1;
 }
 
-int WebServer::Query_Email(struct QUERY_INFO query_info)
+int Query_Email(struct QUERY_INFO query_info, MYSQL mysql)
 {
     return 1;
 }
 
-int WebServer::Pull_Email(struct PULL_INFO pull_info)
+int Pull_Email(struct PULL_INFO pull_info, MYSQL mysql)
 {
     return 1;
 }
@@ -237,4 +274,6 @@ int main()
 {
     WebServer webserver;
     webserver.Run();
+    cout << "(INFO)Main: Exit!" << endl;
+    return 0;
 }
--- Code/WebMail/ThreadPool/ThreadPool.cpp
+++ Code/WebMail/ThreadPool/ThreadPool.cpp
@@ -6,6 +6,9 @@
 #include <mutex>
 #include <condition_variable>
 #include <unistd.h>
+#include <future>
+#include <stdexcept>
+
 using namespace std;
 typedef function<void()> Task;
 
@@ -14,17 +17,19 @@ class ThreadPool
 public:
     ThreadPool(int threads_size = 5);
     ~ThreadPool();
+    void start();
     void stop();
-    void addTask(Task);
+
+    template <class T, class... Args>
+    void addTask(T &&, Args &&...);
 
 private:
-    void start();
     void threadLoop();
     Task take_task();
     int threads_size;
+    bool running;
     mutex m_mutex;
     condition_variable m_cond; //条件变量
-    bool m_started;            //是否开始
 
     vector<thread *> thread_pool; //线程池
     queue<Task> tasks_queue;      //任务队列
@@ -33,20 +38,16 @@ private:
 ThreadPool::ThreadPool(int threads_size)
 {
     this->threads_size = threads_size;
-    start();
+    running = true;
 }
 
 ThreadPool::~ThreadPool()
 {
-    if (m_started)
-    {
-        stop();
-    }
+    stop();
 }
 
 void ThreadPool::start()
 {
-    m_started = true;
     thread_pool.reserve(threads_size);
     for (int i = 0; i < threads_size; i++)
     {
@@ -56,11 +57,10 @@ void ThreadPool::start()
     }
 }
 
-//线程池终止
 void ThreadPool::stop()
 {
+    running = false;
     std::unique_lock<std::mutex> lock(m_mutex);
-    m_started = false;
     m_cond.notify_all();
 
     for (auto it = thread_pool.begin(); it != thread_pool.end(); ++it)
@@ -71,10 +71,9 @@ void ThreadPool::stop()
     thread_pool.clear();
 }
 
-//线程池循环
 void ThreadPool::threadLoop()
 {
-    while (true)
+    while (running)
     {
         Task task = take_task();
         if (task)
@@ -82,26 +81,28 @@ void ThreadPool::threadLoop()
     }
 }
 
-void ThreadPool::addTask(Task task)
+template <class T, class... Args>
+void ThreadPool::addTask(T &&t, Args &&...args)
 {
+    // using RetType = decltype(t(args...));
+    auto task = std::make_shared<std::packaged_task<int()>>(
+        std::bind(std::forward<T>(t), std::forward<Args>(args)...));
+
     unique_lock<mutex> lock(m_mutex);
-    tasks_queue.push(task);
+    tasks_queue.emplace([task]()
+                        { (*task)(); });
     m_cond.notify_one();
 }
 
 Task ThreadPool::take_task()
 {
-    // cout << "1" << endl;
     unique_lock<mutex> lock(m_mutex);
-
-    while (tasks_queue.empty())
+    while (tasks_queue.empty() && running)
     {
-        cout << this_thread::get_id() << endl;
         m_cond.wait(lock);
     }
-    cout << 3 << endl;
     Task task;
-    if (!tasks_queue.empty() && m_started)
+    if (!tasks_queue.empty() && running)
     {
         task = tasks_queue.front();
         tasks_queue.pop();
@@ -109,20 +110,24 @@ Task ThreadPool::take_task()
     return task;
 }
 
-void MyFunc()
-{
-    cout << " at thread[" << this_thread::get_id() << "] output " << endl;
-}
+// void MyFunc(int a, int b)
+// {
+//     // cout << a << endl;
+//     // cout << " at thread[" << this_thread::get_id() << "] output " << endl;
+// }
 
-int main()
-{
-    ThreadPool tp;
-    //这里只添加四次任务，因此任务队列实际大小为4
-    // for (int i = 0; i < 4; ++i)
-    // {
-    //     tp.addTask(MyFunc);
-    // }
-    sleep(10);
-    cout << "exit(0);" << endl;
-    return 0;
-}
+// int main()
+// {
+//     ThreadPool tp;
+//     tp.start();
+//     //这里只添加四次任务，因此任务队列实际大小为4
+//     for (int i = 0; i < 4; ++i)
+//     {
+//         tp.addTask(&MyFunc, 3, i);
+//     }
+//     sleep(3);
+//     // tp.addTask(MyFunc);
+//     sleep(3);
+//     cout << "exit(0);" << endl;
+//     return 0;
+// }
